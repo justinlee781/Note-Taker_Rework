53157762c81eb1669976f9a006ad5f1a7987094d
Justin Lee <98000043+justinlee781@users.noreply.github.com>
2022-08-07 08:35:27 -0700

    commit to main

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./Routes/apiRoutes');
+const htmlRoutes = require('./Routes/htmlRoutes');
 
 const app = express();
 
